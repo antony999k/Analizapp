130d50365b0ece2e6f8dfae26c3561c8c174c72c
javicuriel <javiercuriel94@gmail.com>
2018-11-22 23:50:47 -0600

    Added image id to response

--- controllers/image.controller.js
+++ controllers/image.controller.js
@@ -7,6 +7,7 @@ const db = require('./database.controller');
 exports.getImages = (req, res, next) => {
     db.query(
         'SELECT \
+            i.id as id, \
             m.nombre as metal, \
             e.nombre as experimento, \
             i.tiempo_minutos as tiempo, \
