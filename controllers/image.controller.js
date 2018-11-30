2a51b722c0830a318c196a326efcc2c373cee2fb
javicuriel <javiercuriel94@gmail.com>
2018-11-29 18:04:30 -0600

    Added return id

--- controllers/image.controller.js
+++ controllers/image.controller.js
@@ -51,9 +51,9 @@ exports.analyzeImage = (req, res, next) => {
                 status: 200,
                 name: 'OK',
                 message: 'Imagen analizada correctamente',
-                results: {
-                    id: results[0]["insertId"]
-                }
+                results: [
+                    {id: results[0]["insertId"]}
+                ]
             });
         });
     }));
